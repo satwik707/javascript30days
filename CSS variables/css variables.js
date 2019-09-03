897b240dd3c404036a82d593a369f97211eae596
Satwik Srivastava <41414390+satwik707@users.noreply.github.com>
2019-09-04 00:15:22 +0530

    Update css variables.js

--- CSS variables/css variables.js
+++ CSS variables/css variables.js
@@ -1,9 +1,9 @@
-const inputs=document.querySelectorAll('.controls input');
-function update()
+const inputs=document.querySelectorAll('.controls input');  //take all the inputs ie base blur and spacing
+function update() 
 {
-  console.log(this);
- const suffix=this.dataset.sizing||'';
-  document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+  console.log(this);  //as input was passed in as paramenter so this will give the input div tha has been  passed to the function
+ const suffix=this.dataset.sizing||'';  //we need the suffix ie px in case of blur and spacing or else '' in case of base  
+  document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);  //change property 
 }
-inputs.forEach(input=>input.addEventListener('change',update));
+inputs.forEach(input=>input.addEventListener('change',update));   //use foreach to loop through all the inputs and pass the particular input to the function which has "change"
 inputs.forEach(input=>input.addEventListener('mousemove',update));
